2c229889c3c361d91e532badfde261b21ec76eca
Haytham El-Gebaly <hsmgebaly@gmail.com>
2023-06-22 19:52:30 -0700

    sad face

--- monty.h
+++ monty.h
@@ -40,7 +40,7 @@ typedef struct instruction_s
 {
 	char *opcode;
 	void (*f)(stack_t **stack, unsigned int line_number);
-} instruction_t
+} instruction_t;
 
 /* 2nd THE PRIMARY FUNCTIONS OF INTERPRETER */
 
